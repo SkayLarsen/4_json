7a066dba84607abf28bf1b85486459c97c86590d
SkayLarsen <SkayLarsen@gmail.com>
2017-01-03 21:00:08 +0300

    задача #4

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import os.path
+import sys
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as data_file:
+            return json.load(data_file)
+    else:
+        return None
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    json_data = load_data(sys.argv[1])
+    if json_data:
+        pretty_print_json(json_data)
+    else:
+        print("Не удалось прочитать файл")
